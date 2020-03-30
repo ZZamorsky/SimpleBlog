3d6c7d0a86a2312642732ba964c33db4d717226b
Pavel Mlcuch <pavel.mlcuch@gmail.com>
2020-03-30 23:18:17 +0200

    UI continuing - EC6

--- src/main/resources/static/users.js
+++ src/main/resources/static/users.js
@@ -1,40 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
     loadUsers();
-    document.getElementById('another-button').addEventListener('click', function() {
-        storeUser(document.mainForm.firstname.value, document.mainForm.lastname.value);
-    });
+    document.getElementById('another-button').addEventListener('click',
+        () => storeUser(document.mainForm.firstname.value, document.mainForm.lastname.value));
 });
 
-function storeUser(firstName, lastName) {
-    var req = new XMLHttpRequest();
+const storeUser = (firstName, lastName) => {
+    const req = new XMLHttpRequest();
     req.addEventListener('load', loadUsers);
     req.open("POST", "./api/users/");
     req.setRequestHeader('Content-Type', 'application/json');
     req.send(JSON.stringify({
         name: `${firstName} ${lastName}`
     }));
-}
+};
 
-function loadUsers() {
-    var req = new XMLHttpRequest();
-    req.addEventListener('load', function() {
-        var tableBody = document.getElementById('user-table');
+const loadUsers = () => {
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', () => {
+        const tableBody = document.getElementById('user-table');
         tableBody.innerHTML = '';
-        var users = JSON.parse(this.responseText);
-        users.forEach(function(user) {
-            createRow(tableBody, user.id, user.name);
-        });
+        const users = JSON.parse(req.responseText);
+        users.forEach(user => createRow(tableBody, user.id, user.name));
     });
     req.open("GET", "./api/users/");
     req.send();
-}
+};
 
-function createRow(tableBody, id, name) {
-    var idCell = document.createElement('td');
+const createRow = (tableBody, id, name) => {
+    const idCell = document.createElement('td');
     idCell.innerText = id;
-    var nameCell = document.createElement('td');
+    const nameCell = document.createElement('td');
     nameCell.innerText = name;
-    var userRow = document.createElement('tr');
+    const userRow = document.createElement('tr');
     userRow.append(idCell, nameCell);
     tableBody.append(userRow);
-}
+};
